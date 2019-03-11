a3d06d39e9457d0ec2bd560787d4bd9aa4e905a9
Mazahir Hahimli <mazahir89@gmail.com>
2019-03-11 16:22:16 +0100

    itemPrice Saga implemented to application

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,3 +2,4 @@ export { currentUserSaga } from "./currentUserSaga";
 export { fetchCartSaga } from "./fetchCartSaga";
 export { itemDetailsSaga } from "./itemDetailsSaga";
 export { itemQuantitySaga } from "./itemQuantitySaga";
+export { itemPriceSaga } from "./itemPriceSaga";
