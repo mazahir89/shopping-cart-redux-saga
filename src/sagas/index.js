526ecd8e1e7fd9d9d4505117dc89ade8dc66be8b
Mazahir Hahimli <mazahir89@gmail.com>
2019-03-11 15:59:07 +0100

    item quantity saga. Fetching quantity status to application

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,4 @@
 export { currentUserSaga } from "./currentUserSaga";
 export { fetchCartSaga } from "./fetchCartSaga";
 export { itemDetailsSaga } from "./itemDetailsSaga";
+export { itemQuantitySaga } from "./itemQuantitySaga";
